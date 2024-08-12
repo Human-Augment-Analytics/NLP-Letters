2ad865fb65eec0ad4d2417b4c24103cba3b418f2
benjamyu <benjamyu@twitch.tv>
2024-08-12 01:22:33 -0700

    sort by unnormalized cond prob

--- gender_stats.py
+++ gender_stats.py
@@ -22,7 +22,7 @@ def get_adjectives(text):
     tagged_words = pos_tag(words)
     # Filter adjectives (JJ, JJR, JJS are POS tags for adjectives)
     adjectives = [word for word, pos in tagged_words if pos in ['JJ', 'JJR', 'JJS']]
-    return adjectives
+    return adjectives, len(words)
 
 def get_adverbs(text):
     # Tokenize the text into words
@@ -31,11 +31,12 @@ def get_adverbs(text):
     tagged_words = pos_tag(words)
     # Filter adjectives (JJ, JJR, JJS are POS tags for adjectives)
     adverbs = [word for word, pos in tagged_words if pos in ['RB', 'RBR', 'RBS', 'WRB']]
-    return adverbs
+    return adverbs, len(words)
 
 
 # Load dataset
 df = pd.read_csv(dataset_path, encoding='unicode_escape')
+total_adv_counts = Counter()
 
 female_letters = df.loc[df[label_column] == labels[0]]
 female_adj_counts = Counter()
@@ -46,18 +47,39 @@ male_adj_counts = Counter()
 male_adv_counts = Counter()
 
 
+female_wc = 0
 for x in range(len(female_letters[text_column])):
-    adjs = get_adverbs(female_letters[text_column].iloc[x])
+    adjs, wc = get_adverbs(female_letters[text_column].iloc[x])
     female_adv_counts.update(adjs)
+    female_wc += wc
 
-print("Top 25 adverbs in female applicant text: {}".format(female_adv_counts.most_common(25)))
-
+male_wc = 0
 for x in range(len(male_letters[text_column])):
-    adjs = get_adverbs(male_letters[text_column].iloc[x])
+    adjs, wc= get_adverbs(male_letters[text_column].iloc[x])
     male_adv_counts.update(adjs)
+    male_wc += wc
 
-print("Top 25 adverbs in male applicant text: {}".format(male_adv_counts.most_common(25)))
+total_wc = 0
+for x in range(len(df[text_column])):
+    adjs, wc = get_adverbs(df[text_column].iloc[x])
+    total_adv_counts.update(adjs)
+    total_wc += wc
 
+female_pcts = []
+for w,c in female_adv_counts.items():
+    if total_adv_counts[w] > 10:
+        female_pcts.append((w, (female_adv_counts[w]/female_wc)/(total_adv_counts[w]/total_wc)))
+
+female_pcts = sorted(female_pcts, key=lambda tup: tup[1],reverse=True)
+print("Top Female adv by cond. prob: {}".format(female_pcts[:25]))
+
+male_pcts = []
+for w,c in male_adv_counts.items():
+    if total_adv_counts[w] > 10:
+        male_pcts.append((w, (male_adv_counts[w]/male_wc)/(total_adv_counts[w]/total_wc)))
+
+male_pcts = sorted(male_pcts, key=lambda tup: tup[1],reverse=True)
+print("Top Male adv by cond. prob: {}".format(male_pcts[:25]))
 
 #for x in range(len(female_letters[text_column])):
 #    adjs = get_adjectives(female_letters[text_column].iloc[x])
